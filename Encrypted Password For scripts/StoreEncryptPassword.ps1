0c07c4ff1c1e2bafaff3e39c0ef155146ca95a27
TerasGuy <123095157+TerasGuy@users.noreply.github.com>
2025-01-01 11:25:41 +0200

    Add files via upload

---
+++ Encrypted Password For scripts/StoreEncryptPassword.ps1
@@ -0,0 +1,14 @@
+﻿# Set the password File location
+param (
+    [string]$SecurePasswordFilePath = '.\SecurePassword.xml'
+)
+
+# Get the password from the user
+$RESTAPIPassword = Read-Host -Prompt "Enter the REST API password" -AsSecureString
+
+# Convert the secure password to plain text
+$PlainTextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($RESTAPIPassword))
+
+# Encrypt the password and save it to a file
+$SecurePassword = ConvertTo-SecureString -String $PlainTextPassword -AsPlainText -Force
+$SecurePassword | Export-CliXml -Path $SecurePasswordFilePath
